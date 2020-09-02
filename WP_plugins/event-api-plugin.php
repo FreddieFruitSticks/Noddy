a08dce9ba13ebdfb009e5c082d96085c1534af38
freddie <freddieodonnell@gmail.com>
2020-09-02 20:27:52 +0200

    feature: create party

--- WP_plugins/event-api-plugin.php
+++ WP_plugins/event-api-plugin.php
@@ -33,11 +33,10 @@ function event_api_return_data( WP_REST_Request $request ) {
 
 function create_party_api( WP_REST_Request $request ) {    
     $parameters = $request->get_json_params();
- 
     
-    $a = create_party_db();
+    $a = create_party_db($parameters);
     
-    $response = new WP_REST_Response( $a );
+    $response = new WP_REST_Response( count($parameters['kids']) );
     
     $response->set_status( 200 );
     
@@ -127,11 +126,11 @@ add_action( 'rest_api_init', function () {
     return $event_id;
   };
   
-  function create_party_db() {
+  function create_party_db($parameters) {
     global $wpdb;
     
     $event_id = wp_insert_post( array(
-        'post_title'    => 'Ryan',
+        'post_title'    => $parameters['name'],
         'post_type'  => 'party',
         'post_status'   => 'draft',
         'comment_status'   => 'closed',
@@ -145,40 +144,174 @@ add_action( 'rest_api_init', function () {
       $field_eventid = $wpdb->get_var( $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_excerpt = %s", 'eventid'));
       $field_cell = $wpdb->get_var( $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_excerpt = %s", 'cell_number'));
       
+      $field_children = $wpdb->get_var( $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_excerpt = %s", 'children'));
+      $field_children_age = $wpdb->get_var( $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_excerpt = %s", 'child_age'));
+      $field_children_name = $wpdb->get_var( $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_excerpt = %s", 'child_name'));
+      $field_children_has_gift = $wpdb->get_var( $wpdb->prepare( "SELECT post_name FROM $wpdb->posts WHERE post_excerpt = %s", 'has_gift'));
+            
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'party_name',
+          'meta_value' => $parameters['name']
+      ),
+      array('%d','%s','%s'));      
+        
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_party_name',
+          'meta_value' => $field_name
+      ),
+      array('%d','%s','%s'));        
+        
+        
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'number_of_adults',
+          'meta_value' => $parameters['adults']
+      ),
+      array('%d','%s','%d'));        
+        
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_number_of_adults',
+          'meta_value' => $field_adults
+      ),
+      array('%d','%s','%s'));
+        
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'event_date',
+          'meta_value' => str_replace('-','',$parameters['date'])
+      ),
+      array('%d','%s','%d'));        
+          
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_event_date',
+          'meta_value' => $field_date
+      ),
+      array('%d','%s','%s'));
+          
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'email',
+          'meta_value' => $parameters['email']
+      ),
+      array('%d','%s','%s'));        
+            
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_email',
+          'meta_value' => $field_email
+      ),
+    array('%d','%s','%s'));
+            
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'eventid',
+          'meta_value' => $parameters['eventId']
+      ),
+      array('%d','%s','%d'));        
+              
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_eventid',
+          'meta_value' => $field_eventid
+      ),
+      array('%d','%s','%s'));            
+            
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'cell_number',
+          'meta_value' => $parameters['cell']
+      ),
+      array('%d','%s','%s'));        
+              
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_cell_number',
+          'meta_value' => $field_cell
+      ),
+      array('%d','%s','%s'));
+      
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => 'children',
+          'meta_value' => count($parameters['kids'])
+      ),
+      array('%d','%s','%d'));      
+        
+    $wpdb->insert(
+      $wpdb->postmeta,
+      array(
+          'post_id' => $event_id,
+          'meta_key' => '_children',
+          'meta_value' => $field_children
+      ),
+      array('%d','%s','%s'));     
+          
+    for ($x = 0; $x < count($parameters['kids']); $x++) {
+      $count = strval($x);
+    
       $wpdb->insert(
         $wpdb->postmeta,
         array(
             'post_id' => $event_id,
-            'meta_key' => 'party_name',
-            'meta_value' => 'Ryan'
+            'meta_key' => "children_{$count}_has_gift",
+            'meta_value' => $parameters['kids'][$count]['hasGift']
         ),
         array('%d','%s','%s'));      
-        
-        $wpdb->insert(
+          
+      $wpdb->insert(
         $wpdb->postmeta,
         array(
             'post_id' => $event_id,
-            'meta_key' => '_party_name',
-            'meta_value' => $field_name
+            'meta_key' => "_children_{$count}_has_gift",
+            'meta_value' => $field_children_has_gift
         ),
-        array('%d','%s','%s'));        
+        array('%d','%s','%s'));
         
-        
-        $wpdb->insert(
+      $wpdb->insert(
         $wpdb->postmeta,
         array(
             'post_id' => $event_id,
-            'meta_key' => 'number_of_adults',
-            'meta_value' => 2
+            'meta_key' => "children_{$count}_child_name",
+            'meta_value' => $parameters['kids'][$count]['name']
         ),
-        array('%d','%s','%d'));        
-        
-        $wpdb->insert(
+        array('%d','%s','%s'));      
+          
+      $wpdb->insert(
         $wpdb->postmeta,
         array(
             'post_id' => $event_id,
-            'meta_key' => '_number_of_adults',
-            'meta_value' => $field_adults
+            'meta_key' => "_children_{$count}_child_name",
+            'meta_value' => $field_children_name
         ),
         array('%d','%s','%s'));
         
@@ -186,75 +319,21 @@ add_action( 'rest_api_init', function () {
           $wpdb->postmeta,
           array(
               'post_id' => $event_id,
-              'meta_key' => 'event_date',
-              'meta_value' => 20201016
+              'meta_key' => "children_{$count}_child_age",
+              'meta_value' => $parameters['kids'][$count]['age']
           ),
-          array('%d','%s','%d'));        
-          
-          $wpdb->insert(
+          array('%d','%s','%d'));      
+            
+        $wpdb->insert(
           $wpdb->postmeta,
           array(
               'post_id' => $event_id,
-              'meta_key' => '_event_date',
-              'meta_value' => $field_date
+              'meta_key' => "_children_{$count}_child_age",
+              'meta_value' => $field_children_age
           ),
-          array('%d','%s','%s'));
-          
-          $wpdb->insert(
-            $wpdb->postmeta,
-            array(
-                'post_id' => $event_id,
-                'meta_key' => 'email',
-                'meta_value' => 'freddieo@email.com'
-            ),
-            array('%d','%s','%s'));        
-            
-            $wpdb->insert(
-            $wpdb->postmeta,
-            array(
-                'post_id' => $event_id,
-                'meta_key' => '_email',
-                'meta_value' => $field_email
-            ),
-            array('%d','%s','%s'));
-            
-            $wpdb->insert(
-              $wpdb->postmeta,
-              array(
-                  'post_id' => $event_id,
-                  'meta_key' => 'eventid',
-                  'meta_value' => 32
-              ),
-              array('%d','%s','%d'));        
-              
-              $wpdb->insert(
-              $wpdb->postmeta,
-              array(
-                  'post_id' => $event_id,
-                  'meta_key' => '_eventid',
-                  'meta_value' => $field_eventid
-              ),
-              array('%d','%s','%s'));            
-            
-              $wpdb->insert(
-              $wpdb->postmeta,
-              array(
-                  'post_id' => $event_id,
-                  'meta_key' => 'cell_number',
-                  'meta_value' => "0824445672"
-              ),
-              array('%d','%s','%s'));        
-              
-              $wpdb->insert(
-              $wpdb->postmeta,
-              array(
-                  'post_id' => $event_id,
-                  'meta_key' => '_cell_number',
-                  'meta_value' => $field_cell
-              ),
-              array('%d','%s','%s'));
-          
-        
+          array('%d','%s','%s'));  
+    }
+    
     return $event_id;
   };
   
