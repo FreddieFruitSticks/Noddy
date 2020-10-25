2e1e5ead5ef010f5bf45542caa31c40056fed88c
freddie <freddieodonnell@gmail.com>
2020-10-25 20:28:41 +0200

    fix: some email crap

--- src/lib/home/index.tsx
+++ src/lib/home/index.tsx
@@ -3,7 +3,6 @@ import BookingCalender from "./components/booking-calender"
 import { IHomePage } from "../../../pages"
 
 const Home = ({state, dispatch, events} : IHomePage) => {
-    console.log(events)
     return (
         <div className="ml-5">
             {events?.map(event => {
