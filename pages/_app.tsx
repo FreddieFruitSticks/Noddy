85ef07ad40501812ea8adde834da73c02f903f18
freddie <freddieodonnell@gmail.com>
2020-12-13 13:49:09 +0200

    fix: dl font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ const Noddy = ({ Component, pageProps }) => {
     <div>
       <Head>
         <title>Noddy Charity Chirstmas Party</title>
-        <link rel="preload" href="/_next/static/media/BalooTamma2-Medium.769b702c2a792b8c5be00b8ea60aa1c7.ttf" crossOrigin="anonymous" type="font/ttf" as="font"/>
+        <link rel="preload" href="/_next/static/media/BalooTamma2-Medium.8940f3fac545ecd32c05ce9edc6f3f58.ttf" crossOrigin="anonymous" type="font/ttf" as="font"/>
         <link rel="preload" href="/noddy-party.svg" as="image"/>
         <link rel="preload" href="/load.svg" as="image"/>
         <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&display=swap" async defer></script>
